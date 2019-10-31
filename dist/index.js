26f6f0fa92c998a3d427956179329c1a48c57228
Nahiyan Kamal <noyanayihan@gmail.com>
2019-10-31 01:25:45 +0100

    1.1.5: remove dist folder from source code (#34)
    ### Fixed
    - removing dist folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var console_table_printer_1 = require("./src/console-table-printer");
-exports.Table = console_table_printer_1.Table;
-var internal_table_printer_1 = require("./src/internal-table-printer");
-exports.printTable = internal_table_printer_1.printSimpleTable;
-//# sourceMappingURL=index.js.map
--- dist/src/internal-table-printer.d.ts
+++
@@ -1,5 +0,0 @@
-import { TableInternal } from "./internal-table";
-export declare function printTableTest(table: TableInternal): string[];
-export declare function printTable(table: TableInternal): void;
-export declare function printSimpleTableTest(rows: any[]): string[];
-export declare function printSimpleTable(rows: any[]): void;
