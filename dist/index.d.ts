14f54850a4b857d8550a22a024129157dd20e5c4
Nahiyan Kamal <noyanayihan@gmail.com>
2019-10-31 01:24:11 +0100

    removing dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Table } from './src/console-table-printer';
-export { printSimpleTable as printTable } from './src/internal-table-printer';
--- dist/src/colored-console-line.d.ts
+++
@@ -1,7 +0,0 @@
-import { COLOR } from "./table-constants";
-export declare class ColoredConsoleLine {
-    text: string;
-    constructor();
-    addWithColor(color: COLOR, text: string): void;
-    printConsole(): string;
-}
