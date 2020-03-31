0b6313e3bb1a217cc4b25941766b86261dd5e2fa
Nahiyan Kamal <noyanayihan@gmail.com>
2020-04-01 00:52:03 +0200

    readme update with details

--- test/columnColor.test.ts
+++ test/columnColor.test.ts
@@ -5,43 +5,67 @@ describe('Example: Print a simple Table wiht column colors', () => {
     //Create a table
     const p = new Table({
       columns: [
-        { name: 'index', alignment: 'left', color: 'red' },
-        { name: 'text', alignment: 'right' },
-        { name: 'value', color: 'green' },
+        { name: 'red_left_align_index', alignment: 'left', color: 'red' },
+        { name: 'right_align_text', alignment: 'right' },
+        { name: 'green_value', color: 'green' },
       ],
     });
 
     //add rows with color
     p.addRow(
-      { index: 2, text: 'I would like some blue poison please', value: 10.212 },
+      {
+        red_left_align_index: 2,
+        right_align_text: 'This row is blue',
+        green_value: 10.212,
+      },
       { color: 'blue' }
     );
     p.addRow(
-      { index: 3, text: 'I would like some red wine please', value: 10.212 },
+      {
+        red_left_align_index: 3,
+        right_align_text: 'I would like some red wine please',
+        green_value: 10.212,
+      },
       { color: 'red' }
     );
     p.addRow(
-      { index: 4, text: 'I would like some cyan wine please', value: 10.212 },
+      {
+        red_left_align_index: 4,
+        right_align_text: 'I would like some cyan wine please',
+        green_value: 10.212,
+      },
       { color: 'cyan' }
     );
     p.addRow(
       {
-        index: 5,
-        text: 'I would like some white_bold wine please',
-        value: 10.212,
+        red_left_align_index: 5,
+        right_align_text: 'I would like some white_bold wine please',
+        green_value: 10.212,
       },
       { color: 'white_bold' }
     );
     p.addRow(
-      { index: 6, text: 'I would like some crimson sky please', value: 10.212 },
+      {
+        red_left_align_index: 6,
+        right_align_text: 'I would like some crimson sky please',
+        green_value: 10.212,
+      },
       { color: 'crimson' }
     );
     p.addRow(
-      { index: 7, text: 'I would like some green gemuse please', value: 20.0 },
+      {
+        red_left_align_index: 7,
+        right_align_text: 'I would like some green gemuse please',
+        green_value: 20.0,
+      },
       { color: 'green' }
     );
     p.addRow(
-      { index: 8, text: 'I would like some gelb bananen bitte', value: 100 },
+      {
+        red_left_align_index: 8,
+        right_align_text: 'I would like some gelb bananen bitte',
+        green_value: 100,
+      },
       { color: 'yellow' }
     );
 
