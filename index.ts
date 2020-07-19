0780c7682e0f89572d460540de1294112e5f1745
Nahiyan Kamal <noyanayihan@gmail.com>
2020-07-19 22:24:29 +0200

    fix: expose alignment types

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import Table from './src/console-table-printer';
 import { printSimpleTable as printTable } from './src/internalTable/internal-table-printer';
 
-import { COLOR } from './src/utils/table-constants';
+import { COLOR, ALIGNMENT } from './src/utils/table-constants';
 
-export { Table, printTable, COLOR };
+export { Table, printTable, COLOR, ALIGNMENT };
