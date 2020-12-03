c67d8d0fabd62fe71ed6aff31462be2318e1361c
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-04 00:33:30 +0100

    feat: adding all cases for ansi striping

--- src/utils/ansi-color-stripper.ts
+++ src/utils/ansi-color-stripper.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-control-regex */
-const color8Regex = /\x1b\[\d{1,2}m/g; // \u001b[30m
-const color16Regex = /\x1b\[\d{1,2};1m/g; // \u001b[30;1m
-const color256Regex = /\x1b\[48;5;\d{1,3}m/g; // \u001b[30;1m
+const color8Regex = /\x1b\[\d{1,3}m/g; // \x1b[30m  \x1b[305m
+const color16Regex = /\x1b\[\d{1,2};1m/g; // \x1b[30;1m
+const color256Regex = /\x1b\[48;5;\d{1,3}m/g; // \x1b[48;5;101m
 
 const stripAnsi = (str: string): string => {
   return str
