4416410c6f08dbf5a81d46a51def9f0b43b5ef8f
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-04 11:11:20 +0100

    fix: test

--- test/utils/stripAnsiColor.test.ts
+++ test/utils/stripAnsiColor.test.ts
@@ -3,14 +3,11 @@ import stripAnsiColor from '../../src/utils/ansi-color-stripper';
 
 describe('Strip Ansi Tester', () => {
   it('Simplest test: chalk', () => {
-    const testFunction = (Fn: any) => {
-      const str = Fn('text');
-      console.log('length', str.length);
-      // eslint-disable-next-line no-restricted-syntax
-      for (const char of str) {
-        console.log(char);
-      }
-    };
-    testFunction(chalk.magentaBright);
+    const str = chalk.magentaBright('text');
+    console.log('length', str.length);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const char of str) {
+      console.log(char);
+    }
   });
 });
