6dd18ba8f2e057daf2c87adc87453c4ebfd162c8
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-04 11:13:19 +0100

    fix: test

--- test/utils/stripAnsiColor.test.ts
+++ test/utils/stripAnsiColor.test.ts
@@ -4,21 +4,21 @@ import stripAnsiColor from '../../src/utils/ansi-color-stripper';
 describe('Strip Ansi Tester', () => {
   it('Simplest test: chalk', () => {
     const str = chalk.magentaBright('text');
-    console.log('magentaBright', str.length);
+    console.log('magentaBright', str.length, str);
     // eslint-disable-next-line no-restricted-syntax
     for (const char of str) {
       console.log(char);
     }
 
     const str1 = chalk.yellow('text');
-    console.log('yellow', str1.length);
+    console.log('yellow', str1.length, str1);
     // eslint-disable-next-line no-restricted-syntax
     for (const char of str1) {
       console.log(char);
     }
 
     const str2 = chalk.bgRedBright('text');
-    console.log('bgRedBright', str2.length);
+    console.log('bgRedBright', str2.length, str2);
     // eslint-disable-next-line no-restricted-syntax
     for (const char of str2) {
       console.log(char);
