2f08fc87fa54a24bd11d4b49a2d325bd883cdb61
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-04 11:30:23 +0100

    chore: minor code refactor

--- src/internalTable/tablePreProcessors.ts
+++ src/internalTable/tablePreProcessors.ts
@@ -1,13 +1,12 @@
+/* eslint-disable no-param-reassign */
 import { TableInternal, ComputedColumn } from './internal-table';
 import { findMaxLenOfColumn, Column, Row } from '../utils/table-helpers';
 
 function createComputedColumnsIfNecessary(table: TableInternal) {
   if (table.computedColumns.length) {
-    // eslint-disable-next-line no-param-reassign
     table.computedColumns.forEach((computedColumn: ComputedColumn) => {
       table.addColumn(computedColumn.name);
       table.rows.forEach((row: Row) => {
-        // eslint-disable-next-line no-param-reassign
         row.text[computedColumn.name] = computedColumn.function(row.text);
       });
     });
@@ -16,7 +15,6 @@ function createComputedColumnsIfNecessary(table: TableInternal) {
 
 function disableColumnsIfNecessary(table: TableInternal) {
   if (table.enabledColumns.length) {
-    // eslint-disable-next-line no-param-reassign
     table.columns = table.columns.filter((col: Column) =>
       table.enabledColumns.includes(col.name)
     );
@@ -25,7 +23,6 @@ function disableColumnsIfNecessary(table: TableInternal) {
 
 function enableColumnsIfNecessary(table: TableInternal) {
   if (table.disabledColumns.length) {
-    // eslint-disable-next-line no-param-reassign
     table.columns = table.columns.filter(
       (col: Column) => !table.disabledColumns.includes(col.name)
     );
@@ -34,7 +31,6 @@ function enableColumnsIfNecessary(table: TableInternal) {
 
 function findMaxColLen(table: TableInternal) {
   table.columns.forEach((column) => {
-    // eslint-disable-next-line no-param-reassign
     column.max_ln = findMaxLenOfColumn(column, table.rows);
   });
 }
@@ -47,9 +43,8 @@ export function preProcessColumns(table: TableInternal) {
 }
 
 export function preProcessRows(table: TableInternal) {
-  const newrows = table.rows
+  const newRows = table.rows
     .filter((r) => table.filterFunction(r.text))
     .sort((r1, r2) => table.sortFunction(r1.text, r2.text));
-  // eslint-disable-next-line no-param-reassign
-  table.rows = newrows;
+  table.rows = newRows;
 }
