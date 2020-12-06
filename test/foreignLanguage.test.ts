1f44f86b5a53f9829d0ffb787fadd1cf53b4b6ff
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-06 11:26:25 +0100

    fix: adding tests

---
+++ test/foreignLanguage.test.ts
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+import { Table } from '../index';
+
+describe('Example: Print a simple Table with cell colors', () => {
+  it('cell colors are working', () => {
+    // Create a table
+    const p = new Table();
+
+    // add rows with color
+    p.addRows([
+      // adding multiple rows are possible
+      {
+        Description: 'Some alphabets 这里是中文这里是中文这里是中文',
+        'Ticket No': 'ISSUE-1231',
+      },
+      {
+        Description: 'Some Summary 这里是中文这里是中文',
+        'Ticket No': 'ISSUE-22222',
+      },
+      {
+        Description: 'Description 这里是中文',
+        'Ticket No': chalk.blue('ISSUE-Foreign'),
+      },
+    ]);
+
+    // print
+    p.printTable();
+  });
+});
