71d5007d37a98eac518f4591e2ccfa8bc69cac04
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-08 12:53:44 +0100

    chore(technical): Updated version of eslint and Fixed linting issues

--- src/utils/console-utils.ts
+++ src/utils/console-utils.ts
@@ -3,12 +3,7 @@ import { wcswidth } from 'simple-wcswidth';
 /* eslint-disable no-control-regex */
 const colorRegex = /\x1b\[\d{1,3}m/g; // \x1b[30m  \x1b[305m
 
-const stripAnsi = (str: string): string => {
-  return str.replace(colorRegex, '');
-};
-
-const findWidthInConsole = (str: string): number => {
-  return wcswidth(stripAnsi(str));
-};
+const stripAnsi = (str: string): string => str.replace(colorRegex, '');
+const findWidthInConsole = (str: string): number => wcswidth(stripAnsi(str));
 
 export default findWidthInConsole;
