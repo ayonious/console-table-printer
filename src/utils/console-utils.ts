a14272f6738310cc21492c38adf71fac06b2a9dd
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-08 09:54:48 +0100

    fix: Asian Languages width fixing with simple-wcswidth

--- src/utils/console-utils.ts
+++ src/utils/console-utils.ts
@@ -1,3 +1,5 @@
+import { wcswidth } from 'simple-wcswidth';
+
 /* eslint-disable no-control-regex */
 const colorRegex = /\x1b\[\d{1,3}m/g; // \x1b[30m  \x1b[305m
 
@@ -6,7 +8,7 @@ const stripAnsi = (str: string): string => {
 };
 
 const findWidthInConsole = (str: string): number => {
-  return stripAnsi(str).length;
+  return wcswidth(stripAnsi(str));
 };
 
 export default findWidthInConsole;
