445b014fd44dd567795af7ac52aa4241ebab5a83
Nahiyan Kamal <noyanayihan@gmail.com>
2021-03-06 13:04:48 +0100

    tests

---
+++ test/utils/string-utils.test.ts
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+import { limitWidth } from '../../src/utils/string-utils';
+
+describe('Example: Print a simple Table with cell colors', () => {
+  it('cell colors are working', () => {
+    expect(limitWidth('as da s', 2)).toMatchObject(['as', 'da', 's']);
+    expect(limitWidth('as', 2)).toMatchObject(['as']);
+    expect(
+      limitWidth(chalk.bgMagenta('magenta'), chalk.bgMagenta('magenta').length)
+    ).toMatchObject([chalk.bgMagenta('magenta')]);
+  });
+});
