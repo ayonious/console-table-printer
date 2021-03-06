981d819e552c60f7d1a4f9f24a83e33a97eb582c
Nahiyan Kamal <noyanayihan@gmail.com>
2021-03-06 16:25:51 +0100

    fix: fixing deployment

--- test/utils/string-utils.test.ts
+++ test/utils/string-utils.test.ts
@@ -5,6 +5,7 @@ describe('Example: Print a simple Table with cell colors', () => {
   it('cell colors are working', () => {
     expect(limitWidth('as da s', 2)).toMatchObject(['as', 'da', 's']);
     expect(limitWidth('as', 2)).toMatchObject(['as']);
+    expect(limitWidth('Hiya!', 5)).toMatchObject(['Hiya!']);
     expect(
       limitWidth(chalk.bgMagenta('magenta'), chalk.bgMagenta('magenta').length)
     ).toMatchObject([chalk.bgMagenta('magenta')]);
