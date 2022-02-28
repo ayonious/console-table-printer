43660c03bda19d3cd0a0120c8e2b37816069fe78
Nahiyan Kamal <noyanayihan@gmail.com>
2022-02-28 17:36:12 +0100

    chore: Added more tests

--- test/foreignLanguage.test.ts
+++ test/foreignLanguage.test.ts
@@ -1,7 +1,7 @@
 import { Table } from '../index';
 
 describe('Example: Print a simple Table with cell colors', () => {
-  it('cell colors are working', () => {
+  it('foreign alphabets are working', () => {
     // Create a table
     const p = new Table();
 
@@ -22,6 +22,100 @@ describe('Example: Print a simple Table with cell colors', () => {
       },
     ]);
 
+    // print
+    p.printTable();
+    expect(p.render()).toMatchSnapshot();
+  });
+
+  it('coloring is working', () => {
+    // Create a table
+    const p = new Table();
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-1231',
+        Description: 'Some alphabets 这里是中文这里是中文这里是中文',
+      },
+      { color: 'blue' }
+    );
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-22222',
+        Description: 'Some Summary 这里是中文这里是中文',
+      },
+      { color: 'red' }
+    );
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-1',
+        Description: 'Description 这里是中文',
+      },
+      { color: 'white_bold' }
+    );
+    // print
+    p.printTable();
+    expect(p.render()).toMatchSnapshot();
+  });
+
+  it('customized border is working', () => {
+    // Create a table
+    const p = new Table({
+      style: {
+        /*
+            Style: (red)
+            ╔══════╦═════╦══════╗
+            ║ hob  ║ foo ║ mia  ║
+            ╟══════╬═════╬══════╢
+            ║ ball ║ fox ║ mama ║
+            ╚══════╩═════╩══════╝
+            */
+        headerTop: {
+          left: '\x1b[31m╔\x1b[0m',
+          mid: '\x1b[31m╦\x1b[0m',
+          right: '\x1b[31m╗\x1b[0m',
+          other: '\x1b[31m═\x1b[0m',
+        },
+        headerBottom: {
+          left: '\x1b[31m╟\x1b[0m',
+          mid: '\x1b[31m╬\x1b[0m',
+          right: '\x1b[31m╢\x1b[0m',
+          other: '\x1b[31m═\x1b[0m',
+        },
+        tableBottom: {
+          left: '\x1b[31m╚\x1b[0m',
+          mid: '\x1b[31m╩\x1b[0m',
+          right: '\x1b[31m╝\x1b[0m',
+          other: '\x1b[31m═\x1b[0m',
+        },
+        vertical: '\x1b[31m║\x1b[0m',
+      },
+    });
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-1231',
+        Description: 'Some alphabets 这里是中文这里是中文这里是中文',
+      },
+      { color: 'blue' }
+    );
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-22222',
+        Description: 'Some Summary 这里是中文这里是中文',
+      },
+      { color: 'red' }
+    );
+
+    p.addRow(
+      {
+        'Ticket No': 'ISSUE-1',
+        Description: 'Description 这里是中文',
+      },
+      { color: 'white_bold' }
+    );
     // print
     p.printTable();
     expect(p.render()).toMatchSnapshot();
