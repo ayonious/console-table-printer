fc5dde92618ecd5d7e9f5305c53fcdf4b88ebdc3
Nahiyan Kamal <noyanayihan@gmail.com>
2022-04-03 13:07:30 +0200

    fix: added readme

--- test/readme/readmeExamples1.test.ts
+++ test/readme/readmeExamples1.test.ts
@@ -31,10 +31,16 @@ describe('Example: 1', () => {
         { name: 'text', alignment: 'right' },
         { name: 'is_priority_today', title: 'Is This Priority?' }, // with Title as separate Text
       ],
+      colorMap: {
+        custom_green: '\x1b[32m', // define customized color
+      },
     });
 
     p.addRow({ index: 1, text: 'red wine', value: 10.212 }, { color: 'green' });
-    p.addRow({ index: 2, text: 'green gemuse', value: 20.0 });
+    p.addRow(
+      { index: 2, text: 'green gemuse', value: 20.0 },
+      { color: 'custom_green' } // your green
+    );
     p.addRow(
       { index: 3, text: 'gelb bananen', value: 100, is_priority_today: 'Y' },
       { color: 'yellow' }
