3db3b264741a5116ab1fcb8a6f6d537c93386b23
Nahiyan Kamal <noyanayihan@gmail.com>
2023-03-09 09:28:52 +0100

    chore: skip tests on circle ci

--- test/utils/string-utils.test.ts
+++ test/utils/string-utils.test.ts
@@ -1,11 +1,19 @@
 import chalk from 'chalk';
 import { textWithPadding, limitWidth } from '../../src/utils/string-utils';
 
+const itOrSkip = () => (process.env.SKIP_CHALK_TESTS ? it.skip : it);
+
 describe('Example: Print a simple Table with cell colors', () => {
   it('cell colors are working', () => {
     expect(limitWidth('as da s', 2)).toMatchObject(['as', 'da', 's']);
     expect(limitWidth('as', 2)).toMatchObject(['as']);
     expect(limitWidth('Hiya!', 5)).toMatchObject(['Hiya!']);
+    expect(
+      limitWidth(chalk.bgMagenta('magenta'), chalk.bgMagenta('magenta').length)
+    ).toMatchObject([chalk.bgMagenta('magenta')]);
+  });
+
+  itOrSkip()('cell colors are working: chalk', () => {
     expect(
       limitWidth(chalk.bgMagenta('magenta'), chalk.bgMagenta('magenta').length)
     ).toMatchObject([chalk.bgMagenta('magenta')]);
