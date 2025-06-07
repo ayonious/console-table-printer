421ba42081ef455f61bfdd28499dc2e812b73185
Nahiyan Kamal <noyanayihan@gmail.com>
2025-06-07 15:55:05 +0200

    test message

--- test/features/customColor/basic.test.ts
+++ test/features/customColor/basic.test.ts
@@ -1,5 +1,5 @@
-import { Table } from '../index';
-import { getTableBody, getTableHeader } from './testUtils/getRawData';
+import { Table } from '../../../index';
+import { getTableBody, getTableHeader } from '../../testUtils/getRawData';
 
 describe('Example: Print a simple Table with Custom column colors', () => {
   it('Custom column colors are working', () => {
@@ -86,41 +86,5 @@ describe('Example: Print a simple Table with Custom column colors', () => {
     // print
     p.printTable();
     expect(p.render()).toMatchSnapshot();
-  });
-
-  it('should make sure each column is what its expected to be', () => {
-    const p = new Table({
-      shouldDisableColors: true,
-      columns: [
-        { name: 'red_left_align_index', alignment: 'left', color: 'red' },
-        { name: 'right_align_text', alignment: 'right' },
-        { name: 'green_value', color: 'green' },
-      ],
-      colorMap: {
-        custom_green: '\x1b[32m', // define customized color
-      },
-    });
-
-    p.addRows([
-      {
-        red_left_align_index: 2,
-        right_align_text: 'This is my defined Green',
-        green_value: 10.212,
-      },
-      {
-        red_left_align_index: 3,
-        right_align_text: 'This row is blue as well',
-        green_value: 10.212,
-      },
-    ]);
-
-    const [renderedHeader, renderedBody] = [getTableHeader(p), getTableBody(p)];
-    expect(renderedHeader).toEqual(
-      '│ red_left_align_index │         right_align_text │ green_value │'
-    );
-    expect(renderedBody).toEqual([
-      '│ 2                    │ This is my defined Green │      10.212 │',
-      '│ 3                    │ This row is blue as well │      10.212 │',
-    ]);
   });
 });
